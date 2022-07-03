1f1910bd9d2f808a350a6076c72add0889a87d76
yongbeomkwak <yongbeomkwak@gmail.com>
2022-07-03 23:12:29 +0900

    DatePicker

--- PracticeBase/TestDataPicker/TestDataPicker/ViewController.swift
+++ PracticeBase/TestDataPicker/TestDataPicker/ViewController.swift
@@ -9,11 +9,20 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var lbPickerTime: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
 
+    @IBAction func changeDatePicker(_ sender: UIDatePicker) {
+        let datePickerView=sender
+        let formatter=DateFormatter()
+        
+        formatter.dateFormat = "yyyy-MM-dd HH:mm EEE"
+        lbPickerTime.text="선택시간: " + formatter.string(from: datePickerView.date)
+        
+    }
 }
 
