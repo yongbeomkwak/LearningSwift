13742f09df66048705e9e7f96870e788be7a8a49
yongbeomkwak <yongbeomkwak@gmail.com>
2022-07-17 16:14:47 +0900

    Auto

---
+++ PracticeBase/AutoLayoutTest/AutoLayoutTest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AutoLayoutTest
+//
+//  Created by yongbeomkwak on 2022/07/17.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
