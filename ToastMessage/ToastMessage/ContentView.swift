67dd1c24e26c0d0ac6718f7da4b90dd12fea03ae
yongbeomkwak <yongbeomkwak@gmail.com>
2022-08-04 21:32:34 +0900

    Create Toast Message Project

---
+++ ToastMessage/ToastMessage/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ToastMessage
+//
+//  Created by yongbeomkwak on 2022/08/04.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ ToastMessage/ToastMessage/ToastMessageApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToastMessageApp.swift
+//  ToastMessage
+//
+//  Created by yongbeomkwak on 2022/08/04.
+//
+
+import SwiftUI
+
+@main
+struct ToastMessageApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
