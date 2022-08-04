17f88444d3c42cbbac5e93dc7edf79686817202e
yongbeomkwak <yongbeomkwak@gmail.com>
2022-08-04 23:02:40 +0900

    Toast Message

--- ToastMessage/ToastMessage/ContentView.swift
+++ ToastMessage/ToastMessage/ContentView.swift
@@ -6,11 +6,111 @@
 //
 
 import SwiftUI
+import PopupView
 
 struct ContentView: View {
+    
+    @State var shouldShowBottomSolidMessage : Bool = false
+    @State var shouldShowBottomToastMessage : Bool = false
+    
+    
+    func createBottomSolidMessage() -> some View {
+        HStack(spacing:10){
+            
+            Image(systemName: "trash")
+                .font(.system(size: 40))
+                .foregroundColor(.white)
+            
+            VStack{
+                Text("토스트 메시지 입니다!")
+                    .font(.system(size: 14))
+                Text("토스트 메시지 입니다!")
+                    .font(.system(size: 14))
+                
+                Divider().opacity(0)
+                
+            }.foregroundColor(.white)
+            
+        }
+        .padding(.vertical, 5)
+        .padding(.horizontal,10)
+        .frame(width:UIScreen.main.bounds.width)
+        .background(.purple)
+        .padding(.bottom, UIApplication.shared.windows.first?.safeAreaInsets.bottom == 0 ? 0 : 15)
+    }
+    
+    func createBottomToastMessage() -> some View {
+        HStack(spacing:10){
+            
+            Image(systemName: "circle")
+                .font(.system(size: 40))
+                .foregroundColor(.white)
+            
+            VStack{
+                Text("토스트 메시지 입니다!")
+                    .font(.system(size: 14))
+                Text("토스트 메시지 입니다!")
+                    .font(.system(size: 14))
+                
+                Divider().opacity(0)
+                
+            }.foregroundColor(.white)
+            
+        }
+        .frame(width:UIScreen.main.bounds.width)
+        .background(.green)
+        
+    }
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        ZStack{
+            VStack(spacing:10){
+                Spacer()
+                Button {
+                    self.shouldShowBottomSolidMessage = true
+                } label: {
+                    Text("바텀 솔리드 메시지")
+                        .font(.system(size: 25))
+                        .fontWeight(.bold)
+                        .foregroundColor(.white)
+                        .padding()
+                        .background(.blue)
+                        .cornerRadius(10)
+                }
+                
+                Button {
+                
+                    self.shouldShowBottomToastMessage = true
+                } label: {
+                    Text("바텀 토스트 메시지")
+                        .font(.system(size: 25))
+                        .fontWeight(.bold)
+                        .foregroundColor(.white)
+                        .padding()
+                        .background(.green)
+                        .cornerRadius(10)
+                }
+                
+                
+                Spacer()
+                
+                
+                
+                
+                
+            }// Vstack
+            .edgesIgnoringSafeArea(.all)
+            
+        }.popup(isPresented: $shouldShowBottomSolidMessage, type: .toast, position: .bottom, animation: .default, autohideIn: 2,closeOnTap: true, closeOnTapOutside: true) {
+            self.createBottomSolidMessage()
+        }
+        .popup(isPresented: $shouldShowBottomToastMessage, type: .floater(verticalPadding: 20), position: .bottom, animation: .spring(), autohideIn: nil,closeOnTap: true, closeOnTapOutside: true,dismissCallback: {
+            print("Dissmiss")
+        }) {
+           
+            self.createBottomToastMessage()
+        }
+        //Zstack
     }
 }
 
