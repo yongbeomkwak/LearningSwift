8084e74bf87ba42dab1bdd7d95fb777477ab09d3
yongbeomkwak <yongbeomkwak@gmail.com>
2022-08-05 23:22:03 +0900

    Create Volume Controler

---
+++ VolumeControl/VolumeControl/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  VolumeControl
+//
+//  Created by yongbeomkwak on 2022/08/05.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ VolumeControl/VolumeControl/VolumeControlApp.swift
@@ -0,0 +1,17 @@
+//
+//  VolumeControlApp.swift
+//  VolumeControl
+//
+//  Created by yongbeomkwak on 2022/08/05.
+//
+
+import SwiftUI
+
+@main
+struct VolumeControlApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
